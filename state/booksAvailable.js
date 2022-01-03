7e345931e77c13afbcf6cb349317812a96d9f0f6
KevinC37 <49222836+KevinC37@users.noreply.github.com>
2022-01-03 17:28:45 +0200

    Update booksAvailable.js

--- state/booksAvailable.js
+++ state/booksAvailable.js
@@ -1,9 +1,9 @@
 import idGenerator from "../utils/idGenerator.js";
 
 const booksAvailable = [
-  { name: 'Production-Ready Microservices', author: 'Susan J. Fowler', price: '$32', imageSrc: 'shoppingList/images/414CRjLjwgL._SX403_BO1,204,203,200_.jpg', id: idGenerator.next().value },
-  { name: 'Release It!', author: 'Michael T. Nygard', price: '$45', imageSrc: 'shoppingList/images/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg', id: idGenerator.next().value },
-  { name: 'React-and-React-Native---Third-Edition', author: 'Adam Boduch , Roy Derks ', price: "$5", imageSrc: 'shoppingList/images/react_and_reactNative.png', id: idGenerator.next().value }
+  { name: 'Production-Ready Microservices', author: 'Susan J. Fowler', price: '$32', imageSrc: 'images/414CRjLjwgL._SX403_BO1,204,203,200_.jpg', id: idGenerator.next().value },
+  { name: 'Release It!', author: 'Michael T. Nygard', price: '$45', imageSrc: 'images/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg', id: idGenerator.next().value },
+  { name: 'React-and-React-Native---Third-Edition', author: 'Adam Boduch , Roy Derks ', price: "$5", imageSrc: 'images/react_and_reactNative.png', id: idGenerator.next().value }
 ]
 
 export default booksAvailable;

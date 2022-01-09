42151f9f1e3e17722f7dd45fc62895ae438e4c15
KevinC37 <49222836+KevinC37@users.noreply.github.com>
2022-01-09 20:50:41 +0200

    fixed bug with History API

--- scripts/index.js
+++ scripts/index.js
@@ -5,35 +5,72 @@ import calculateTotal from '../state/calculateTotal.js';
 import initStore from '../state/initStore.js';
 
 import CartItem from '../components/CartItem.js';
+import BookPresentation from '../components/BookPresentation.js';
 
 
 
 const bookShelf = document.getElementById('book-shelf');
 const addBooksOnShelf = () => booksAvailable.map(e => append(bookShelf, new BookItems(e.id, e.name, e.author, e.price, e.imageSrc)))
+
+const cartItems = JSON.parse(window.localStorage.getItem('booksInCart'));
+export const addItemsToCart = () => cartItems.map(e => {
+  const cartDOM = document.getElementById('shopping_cart');
+  cartDOM.appendChild(new CartItem(e.name, e.price, e.id, e.count))
+})
+
 addBooksOnShelf();
 calculateTotal();
-
-
-
+addItemsToCart();
 
 window.onload = () => {
   initStore();
 
-  const cartItems = JSON.parse(window.localStorage.getItem('booksInCart'));
-  const addItemsToCart = () => cartItems.map(e => {
-    const cartDOM = document.getElementById('shopping_cart');
-    cartDOM.appendChild(new CartItem(e.name, e.price, e.id, e.count))
-  })
+  history.pushState({}, "Home", "/shoppingList");
 
-  history.pushState({}, "Home", "/shoppingList/");
-  addItemsToCart();
 
   window.addEventListener('popstate', e => {
-    console.log(e)
-    if (!e.target.location.hash) {
-      window.history.back();
-      history.pushState({}, "Home", e.target.location);
+
+    if (!window.location.hash) {
+      document.body.innerHTML = `
+    <div id="root">
+      <div class="container" id="book-shelf"></div>
+      <div class="shopping-cart-table">
+        <h2>Your Order</h2>
+        <ul class="shopping-header">
+          <li>#</li>
+          <li>item</li>
+          <li>count</li>
+          <li>price</li>
+          <li>action</li>
+        </ul>
+        <div class="shopping-conatiner" id="shopping_cart"></div>
+        <div class="total">Total: $ <span id="cart-total">100</span></div>
+      </div>
+    </div>
+    <script src="scripts/index.js" type="module"></script>
+  `
+      setTimeout(() => {
+        const bookShelf = document.getElementById('book-shelf');
+        const addBooksOnShelf = () => booksAvailable.map(e => append(bookShelf, new BookItems(e.id, e.name, e.author, e.price, e.imageSrc)))
+
+        const cartItems = JSON.parse(window.localStorage.getItem('booksInCart'));
+        const addItemsToCart = () => cartItems.map(e => {
+          const cartDOM = document.getElementById('shopping_cart');
+          cartDOM.appendChild(new CartItem(e.name, e.price, e.id, e.count))
+        })
+
+        addBooksOnShelf();
+        calculateTotal();
+        addItemsToCart();
+      }, 0);
+    } else {
+      const bookProps = booksAvailable.find(e => e.id === history.state.id);
+      document.getElementById('root').innerHTML = '';
+      document.getElementById('root').appendChild(new BookPresentation(bookProps.id, bookProps.imageSrc,
+        bookProps.name, bookProps.author, bookProps.price, bookProps.description, bookProps.language,
+        bookProps.pages, bookProps.publicationDate, bookProps.publisher, bookProps.rating, bookProps.readingAge));
     }
+
   });
 
 
